eaba81c18e3be2f36c588ab9aeee7bd5c66efbfb
181195ali <46881554+181195ali@users.noreply.github.com>
2019-01-21 10:50:22 +0500

    Add files via upload
    This was the first try, hope I'll perform better next time

---
+++ q1_181195.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Input: ";
+	cin>>n;
+	for(int i=1; i<=(n*2)-1 ; i++)
+	{
+		for(int j=1; j<=(n*2); j++)
+		{
+			cout<<i;
+		
+		}
+		cout<<endl;
+	
+	}
+}
